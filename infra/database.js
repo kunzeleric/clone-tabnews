dcd00b55543a5c679eca9126b005d5e432e3d8a7
Eric Kunzel <kunzel.eric@gmail.com>
2026-06-26 12:38:20 -0300

    chore: created `database.js`, fixed docker scripts and using env variables

---
+++ infra/database.js
@@ -0,0 +1,26 @@
+import { Client } from "pg";
+
+export function createPgClient() {
+  return new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+}
+
+export async function query(queryObject) {
+  const client = createPgClient();
+  await client.connect();
+
+  const result = await client.query(queryObject);
+
+  await client.end();
+
+  return result;
+}
+
+export const database = {
+  query: query,
+};
