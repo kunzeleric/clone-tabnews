c83e362a1f926d2d87d966a187af09dc9b806ed1
Eric Kunzel <kunzel.eric@gmail.com>
2026-09-23 08:43:57 -0300

    chore: add `secretlint` config and `pre-commit` hook

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,6 +4,8 @@ export default async function status(request, response) {
   try {
     const updatedAt = new Date().toISOString();
 
+    const API_KEY = "LiWSsZqqQVdIIX4sODuBu5KLJpZIOhEE";
+
     const databaseVersionResult = await database.query("SHOW server_version;");
     const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
